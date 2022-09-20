9444fc1b71d1ff27ea272d5fbbe5d6f47c9a4ba7
William Mba <wmba@jovaco.ca>
2022-09-20 16:55:54 -0400

    add ag-grid --basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef } from 'ag-grid-community';
+import { CellChangedEvent } from 'ag-grid-community/dist/lib/entities/rowNode';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'x-aggrid-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {
+  }
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
   title = 'xplore-ag-grid';
+
+  style: any = {
+    height: "500px",
+    width: "620px"
+  }
+  rowData$! : Observable<any[]>;
+
+  rowData: any[] = [
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porshe', model: 'Boxster', price: 75000},
+  ];
+  colDefs: ColDef[] = [
+    {field: 'make'},
+    {field: 'model'},
+    {field: 'price'}
+  ];
+
+  defaultColDef: ColDef = {
+    sortable: true, filter: true
+  }
+  ngOnInit(){
+    this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json')
+  }
+
+  onCellClicked(event: CellClickedEvent){
+    console.log(event);
+  }
+
 }
