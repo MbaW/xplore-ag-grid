3629a232234a1ae5dfbe1fb8f097d9d892c8b3cd
William Mba <wmba@jovaco.ca>
2022-09-27 17:58:38 -0400

    add hello and googbye components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataGridModule } from './data-grid/data-grid.module';
+import { HelloComponent } from './hello/hello.component';
+import { GoodByeComponent } from './good-bye/good-bye.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    GoodByeComponent
   ],
   imports: [
     BrowserModule,
